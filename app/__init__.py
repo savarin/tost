bdb12be2a17a281c5145ee0e0bee0cc0c15eec20
Ezzeri Esa <esa@squareup.com>
2017-09-17 13:34:36 -0700

    added comments and fixed indents

--- app/__init__.py
+++ app/__init__.py
@@ -142,39 +142,39 @@ def create_app(config_name):
         return Tost.query.filter_by(tost_id=ppgn.ppgn_tost_id).first()
 
     def review_access_token(access_token, user_id):
-            ppgn = get_ppgn_by_token(access_token)
+        ppgn = get_ppgn_by_token(access_token)
 
-            # case 1: propagation invalid
-            if not ppgn:
-                return 1, None
+        # case 1: propagation invalid
+        if not ppgn:
+            return 1, None
 
-            user_ppgn = get_ppgn_by_user_tost(user_id, ppgn.ppgn_tost_id)
+        user_ppgn = get_ppgn_by_user_tost(user_id, ppgn.ppgn_tost_id)
 
-            # case 2: user visits resource for the first time
-            if not user_ppgn:
-                new_ppgn = Propagation(ppgn.ppgn_tost_id,
-                                       user_id,
-                                       ppgn.ppgn_id,
-                                       ppgn._ppgn_rank+1)
-                new_ppgn.save()
+        # case 2: user visits resource for the first time
+        if not user_ppgn:
+            new_ppgn = Propagation(ppgn.ppgn_tost_id,
+                                   user_id,
+                                   ppgn.ppgn_id,
+                                   ppgn._ppgn_rank+1)
+            new_ppgn.save()
 
-                return 2, new_ppgn._ppgn_token
+            return 2, new_ppgn._ppgn_token
 
-            # case 3: user is creator of tost that propagation points to
-            if ppgn.ppgn_id == user_ppgn.ppgn_id:
-                return 3, access_token
+        # case 3: user is creator of tost that propagation points to
+        if ppgn.ppgn_id == user_ppgn.ppgn_id:
+            return 3, access_token
 
-            # case 4: user propagation is of lower priority than propagation in url
-            if user_ppgn._ppgn_rank > ppgn._ppgn_rank + 1:
-                user_ppgn._ppgn_parent_id = ppgn.ppgn_id
-                user_ppgn._ppgn_rank = ppgn._ppgn_rank + 1
-                user_ppgn.save()
+        # case 4: user propagation is of lower priority than propagation in URL
+        if user_ppgn._ppgn_rank > ppgn._ppgn_rank + 1:
+            user_ppgn._ppgn_parent_id = ppgn.ppgn_id
+            user_ppgn._ppgn_rank = ppgn._ppgn_rank + 1
+            user_ppgn.save()
 
-                return 4, user_ppgn._ppgn_token
-
-            # case 5: user propagation is of higher priority than propagation in url
-            if user_ppgn._ppgn_rank <= ppgn._ppgn_rank + 1:
-                return 5, user_ppgn._ppgn_token
+            return 4, user_ppgn._ppgn_token
+            
+        # case 5: user propagation is of higher priority than propagation in URL
+        if user_ppgn._ppgn_rank <= ppgn._ppgn_rank + 1:
+            return 5, user_ppgn._ppgn_token
 
     @app.route("/tost/<access_token>", methods=["GET", "PUT"])
     @auth.login_required
@@ -195,8 +195,8 @@ def create_app(config_name):
                 return response
 
             # case 2: user visits resource for the first time
-            # case 4: user propagation is of lower priority than propagation in url
-            # case 5: user propagation is of higher priority than propagation in url
+            # case 4: user propagation is of lower priority than propagation in URL
+            # case 5: user propagation is of higher priority than propagation in URL
             if case in [2, 4, 5]:
                 return redirect(url_for("view_tost", access_token=access_token))
 
@@ -220,8 +220,8 @@ def create_app(config_name):
                 return response
 
             # case 2: user visits resource for the first time
-            # case 4: user propagation is of lower priority than propagation in url
-            # case 5: user propagation is of higher priority than propagation in url
+            # case 4: user propagation is of lower priority than propagation in URL
+            # case 5: user propagation is of higher priority than propagation in URL
             if case in [2, 4, 5]:
                 response = jsonify({
                     "code": 50,
@@ -280,6 +280,7 @@ def create_app(config_name):
         src_ppgn = get_ppgn_by_token(src_access_token)
         ppgn = get_ppgn_by_token(access_token)
 
+        # return 400 if user propagation not descendant of propagion in URL
         if not (src_ppgn and ppgn and
                 src_ppgn.ppgn_tost_id == ppgn.ppgn_tost_id and
                 src_ppgn._ppgn_rank >= ppgn._ppgn_rank + 1):
@@ -290,11 +291,12 @@ def create_app(config_name):
             response.status_code = 400
             return response
 
+        # upgrade if user propagation not direct descendant to propagation in URL
         if src_ppgn._ppgn_rank > ppgn._ppgn_rank + 1:
             src_ppgn._ppgn_parent_id = ppgn.ppgn_id
             src_ppgn._ppgn_rank = ppgn._ppgn_rank + 1
 
-            # BFS to find all child propagations
+            # BFS to find all child propagations, then upgrade ranks
             queue = [(src_ppgn, ppgn._ppgn_rank + 1)]
 
             while queue:
@@ -319,6 +321,7 @@ def create_app(config_name):
         src_ppgn = get_ppgn_by_token(src_access_token)
         ppgn = get_ppgn_by_token(access_token)
 
+        # return 400 if user propagation not descendant of propagion in URL
         if not (src_ppgn and ppgn and
                 src_ppgn.ppgn_tost_id == ppgn.ppgn_tost_id and
                 src_ppgn._ppgn_rank >= ppgn._ppgn_rank + 1):
@@ -331,7 +334,7 @@ def create_app(config_name):
 
         src_ppgn._ppgn_disabled = True
 
-        # BFS to find all child propagations
+        # BFS to find all child propagations, then toggle ancestor flag
         queue = [src_ppgn]
 
         while queue:
